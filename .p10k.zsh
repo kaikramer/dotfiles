1fe9a664dd3883b01c0b08aa89b59bada5d8bee3
Kai Kramer <kaikramer@users.noreply.github.com>
2020-01-19 14:10:26 +0100

    p10k: Grey for time

--- .p10k.zsh
+++ .p10k.zsh
@@ -940,7 +940,7 @@
 
   ####################################[ time: current time ]####################################
   # Current time color.
-  typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
+  typeset -g POWERLEVEL9K_TIME_FOREGROUND=243
   # Format for the current time: 09:51:02. See `man 3 strftime`.
   typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
   # If set to true, time will update when you hit enter. This way prompts for the past
