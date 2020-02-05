f7a5a99ff1fdbccb820325620233fcf776ccab09
Kai Kramer <kaikramer@users.noreply.github.com>
2020-02-05 23:48:09 +0100

    fish user color scheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,19 +5,19 @@ set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/*"'
 set -g theme_display_git yes
 set -g theme_display_git_dirty yes
 set -g theme_display_git_untracked no
-set -g theme_display_git_ahead_verbose no
+set -g theme_display_git_ahead_verbose yes
 set -g theme_display_git_dirty_verbose no
-set -g theme_display_git_stashed_verbose no
-set -g theme_display_git_master_branch no
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_master_branch yes
 set -g theme_git_worktree_support no # buggy
 set -g theme_use_abbreviated_branch_name yes
 set -g theme_display_vagrant no
 set -g theme_display_docker_machine no
-set -g theme_display_k8s_context yes
+set -g theme_display_k8s_context no
 set -g theme_display_hg no
 set -g theme_display_virtualenv no
 set -g theme_display_ruby no
-set -g theme_display_nvm yes
+set -g theme_display_nvm no
 set -g theme_display_user ssh
 set -g theme_display_hostname ssh
 set -g theme_display_vi no
@@ -34,11 +34,44 @@ set -g theme_nerd_fonts yes
 set -g theme_show_exit_status yes
 set -g theme_display_jobs_verbose yes
 set -g default_user kk
-set -g theme_color_scheme base16
+#set -g theme_color_scheme base16
 set -g fish_prompt_pwd_dir_length 1
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor no
 set -g theme_newline_prompt '$ '
+
+function bobthefish_colors -S -d 'User defined colors'
+    set -x color_initial_segment_exit     383838 red
+    set -x color_initial_segment_su       383838 green
+    set -x color_initial_segment_jobs     383838 blue
+
+    set -x color_path                     383838 white
+    set -x color_path_basename            383838 white
+    set -x color_path_nowrite             383838 magenta
+    set -x color_path_nowrite_basename    383838 magenta
+
+    set -x color_repo                     383838 green
+    set -x color_repo_work_tree           383838 green --bold
+    set -x color_repo_dirty               383838 green
+    set -x color_repo_staged              383838 yellow
+
+    set -x color_vi_mode_default          383838 brblue
+    set -x color_vi_mode_insert           383838 brgreen
+    set -x color_vi_mode_visual           383838 bryellow
+
+    set -x color_vagrant                  383838 brcyan
+    set -x color_k8s                      383838 magenta
+    set -x color_username                 383838 brgrey
+    set -x color_hostname                 383838 brgrey
+    set -x color_rvm                      383838 brmagenta
+    set -x color_nvm                      383838 brgreen
+    set -x color_virtualfish              383838 brblue
+    set -x color_virtualgo                383838 brblue
+    set -x color_desk                     383838 brblue
+end
+
+set fish_color_command green
+set fish_color_param white
 
 # SSH: set tmux pane title to hostname
 function ssh
