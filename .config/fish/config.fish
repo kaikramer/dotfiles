e965160123a4a4172f02d138e87a8f72691b5ca4
Kai Kramer <kaikramer@users.noreply.github.com>
2023-12-19 22:13:50 +0100

    updated fish and starship config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,48 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
 
-  alias la='ls -la'
-  alias vi='nvim'
-  alias fd='fd --hidden --no-ignore'
+  set -U fish_greeting
 
-  alias sc='openssl x509 -noout -text -inform DER -nameopt RFC2253 -in '
-  alias sp='openssl x509 -noout -text -inform PEM -nameopt RFC2253 -in '
-  alias sl='openssl crl -noout -text -inform DER -in '
-  alias p12='openssl pkcs12 -in '
-  alias p10='openssl req -noout -text -in '
-  alias b64='openssl enc -d -base64 -in '
+  alias cat="batcat -p -P"
+  alias exa="exa -la --git"
+  alias la="ls -la"
+  alias vi="nvim"
+  alias fd="fdfind --hidden --no-ignore "
 
-  alias g='git'
-  alias ga='git add'
-  alias gb='git branch'
-  alias gbv='git branch -vv'
-  alias gc='git commit -v'
-  alias gco='git checkout'
-  alias gd='git difftool'
-  alias gf='git fetch --prune'
-  alias gl='git --no-pager log -25 --pretty=format:"%C(yellow)%h %C(green)%ad%C(auto)%d %Creset%s %C(cyan)%aN" --date=short --graph --decorate'
-  alias gm='git merge'
-  alias gp='git push'
-  alias gr='git reset'
-  alias gs='git status'
+  abbr sc 'openssl x509 -noout -text -inform DER -nameopt RFC2253 -in '
+  abbr sp 'openssl x509 -noout -text -inform PEM -nameopt RFC2253 -in '
+  abbr sl 'openssl crl -noout -text -inform DER -in '
+  abbr p12 'openssl pkcs12 -in '
+  abbr p10 'openssl req -noout -text -in '
+  abbr b64 'openssl enc -d -base64 -in '
 
-  set -x FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/*"'
+  abbr g 'git'
+  abbr ga 'git add'
+  abbr gb 'git branch'
+  abbr gbv 'git branch -vv'
+  abbr gc 'git commit -v'
+  abbr gco 'git checkout'
+  abbr gd 'git difftool'
+  abbr gf 'git fetch --prune'
+  abbr gl 'git --no-pager log -25 --pretty=format:"%C(yellow)%h %C(green)%ad%C(auto)%d %Creset%s %C(cyan)%aN" --date short --graph --decorate'
+  abbr gm 'git merge'
+  abbr gp 'git push'
+  abbr gr 'git reset'
+  abbr gs 'git status'
+
+  set -gx FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/*"'
+
+  # add fzf key bindings
+  function fish_user_key_bindings
+    fzf_key_bindings
+  end
+
+  # ssh completion
+  function fssh -d "Fuzzy-find ssh host and ssh into it"
+    rg --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2- --output-delimiter=\n | sort | uniq -u | fzf | read -l result; and ssh "$result"
+  end
+  bind \cs 'fssh'
+
+  source ~/.work.fish
 
   starship init fish | source
 end
